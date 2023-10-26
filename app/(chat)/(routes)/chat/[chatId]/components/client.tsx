c5ec12f587dae6551ff309bba72247ba1419566c
samdaphbynet <lj8@hotmail.fr>
2023-10-26 12:13:49 +0200

    to handle auto-completion of text in the chat field.

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,7 +1,11 @@
 "use client"
 
+import {useCompletion} from "ai/react"
+import { FormEvent, useState } from "react";
 import { Message, Portfolio } from "@prisma/client"
+import { useRouter } from "next/navigation";
 import { ChatHeader } from "@/components/chat-header";
+import { ChatForm } from "@/components/chat-Form";
 
 interface ChatClientProps {
     portfolio: Portfolio & {
@@ -15,9 +19,50 @@ interface ChatClientProps {
 export const ChatClient = ({
     portfolio
 }: ChatClientProps) => {
+    const router = useRouter();
+    const [messages, setMessages] = useState<any[]>(portfolio.message);
+
+    const {
+        input,
+        isLoading,
+        handleInputChange,
+        handleSubmit,
+        setInput
+    } = useCompletion({
+        api: `/api/chat/${portfolio.id}`,
+        onFinish(prompt, completion) {
+            const systemMessage = {
+                role: 'system',
+                content: completion
+            };
+
+            setMessages((current) => [...current, systemMessage]);
+            setInput("");
+            router.refresh();
+        },
+    });
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        const userMessage = {
+            role: "user",
+            content: input
+        }
+
+        setMessages((current) => [...current, userMessage]);
+        handleSubmit(e);
+    }
+
     return (
         <div className="flex flex-col h-full p-4 space-y-2">
             <ChatHeader portfolio={portfolio} />
+            <div>
+                Chat
+            </div>
+            <ChatForm 
+                isLoading={isLoading}
+                input={input}
+                handleInputChange={handleInputChange}
+                onSubmit={onSubmit}/>
         </div>
     )
 }

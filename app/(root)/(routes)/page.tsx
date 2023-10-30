02917acca1169c6f5515005c9a3728896c2e4636
samdaphbynet <lj8@hotmail.fr>
2023-10-30 11:08:20 +0100

    added section hero

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,24 +1,23 @@
 import { Categories } from "@/components/categories";
 import { Samdaph } from "@/components/samdaph";
 import { SearchInput } from "@/components/search-input";
+import { SectionHero } from "@/components/sectionhero";
 import prismadb from "@/lib/prismadb";
 
 interface RootPageProps {
   searchParams: {
     categoryId: string;
     name: string;
-  }
+  };
 }
 
-export default async function RootPage({
-  searchParams
-}: RootPageProps) {
+export default async function RootPage({ searchParams }: RootPageProps) {
   const data = await prismadb.portfolio.findMany({
     where: {
       categoryId: searchParams.categoryId,
       name: {
-        search: searchParams.name
-      }
+        search: searchParams.name,
+      },
     },
     orderBy: {
       createdAt: "desc",
@@ -26,19 +25,23 @@ export default async function RootPage({
     include: {
       _count: {
         select: {
-          message: true
-        }
-      }
-    }
+          message: true,
+        },
+      },
+    },
   });
 
   const categories = await prismadb.category.findMany();
 
   return (
-    <div className="h-full w-full p-8 md:pl-40 space-y-4 pt-6 pl-6 ">
-      <SearchInput />
-      <Categories data={categories} />
-      <Samdaph data={data}/>
+    <div className="hero-container">
+      {/* <SearchInput /> */}
+
+          <SectionHero />
+      <div className="category">
+          <Categories data={categories} />
+          <Samdaph data={data} />
+      </div>
     </div>
-  )
+  );
 }

6f2d28721bf6b35de6df74a9996109bd1a14461f
samdaphbynet <lj8@hotmail.fr>
2023-11-01 16:48:07 +0100

    delete icon docs

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,25 +15,19 @@ export const Sidebar = () => {
         {
             icon: <Image src="/maison.png" width="40" height="40" alt="test"/>,
             href: '/',
-            label: 'Home',
+            label: 'Accueil',
             pro: false,
         },
         {
             icon: <Image src="/plus.png" width="40" height="40" alt="test"/>,
             href: '/companion/new',
-            label: 'Create',
+            label: 'Créer',
             pro: true,
         },
         {
             icon: <Image src="/reglage.png" width="40" height="40" alt="test"/>,
             href: '/settings',
-            label: 'Settings',
-            pro: false,
-        },
-        {
-            icon: <Image src="/fichier-doc.png" width="40" height="40" alt="test"/>,
-            href: '/Docs',
-            label: 'Docs',
+            label: 'Paramètres',
             pro: false,
         },
 
@@ -45,7 +39,7 @@ export const Sidebar = () => {
 
     return (
         <div className="sidebar">
-            <div className='justify-center p-3 pt-10 flex-1'>
+            <div className='flex justify-center items-center p-3 pt-10 flex-1'>
                 <div className='space-y-2'>
                     {routes.map((route) => (
                         <div onClick={() => onNavigate(route.href, route.pro)} key={route.href} className={cn(

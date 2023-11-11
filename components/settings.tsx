f218332b86bb2e96b8376e6dcaa27c1a183d1b62
samdaphbynet <lj8@hotmail.fr>
2023-11-11 14:30:05 +0100

    fixed style page settings

--- components/settings.tsx
+++ components/settings.tsx
@@ -1,5 +1,9 @@
 export const PageSetting = () => {
     return (
-        <div className="feature-about">Page setting</div>
+        <div className="feature-about">
+            <div className="container">
+                🚧 Désolé pour le désordre ! Cette page est actuellement en cours de construction et sera bientôt prête pour votre exploration. Nous travaillons dur pour ajouter de nouvelles fonctionnalités passionnantes. Merci de votre compréhension et revenez bientôt pour découvrir nos dernières mises à jour ! 🚀
+            </div>
+        </div>
     )
 }
